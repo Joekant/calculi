8d06c44c0e624760200109289f0168404e5d2194
Joel Cantillo <nosoyjoel@NoSoyJoel.local>
2015-01-10 19:10:51 +0100

    index2.php

---
+++ html/dev/_worker/index2.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Calculi</title>
+
+  <!-- build:css ../styles/styles.min.css -->
+  <link rel="stylesheet" href="../css/normalize.css" />
+  <link rel="stylesheet" href="../css/foundation.css" />
+  <link rel="stylesheet" href="../css/app.css" />
+  <!-- endbuild -->
+
+  <!-- build:js ../scripts/scripts_top.min.js -->
+  <script src="../js/vendor/modernizr.js"></script>
+  <script src="../js/vendor/jquery.js"></script>
+  <!-- endbuild -->
+
+  <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+  <link href='http://fonts.googleapis.com/css?family=Lato:100,200,300,400,700' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Shadows+Into+Light+Two' rel='stylesheet' type='text/css'>
+  <link rel="icon" href="../img/favicon.ico" type="image/x-icon">
+</head>
+<body>
+
+  <header class="show-for-large-up">
+    <div class="icon-bar dark-bg large-horizontal six-up">
+     
+        <a href="#" id="" class="item active">
+          <i class="fa fa-dashboard fa-fw"></i>
+          <label>Dashboard</label>
+        </a>
+    
+    </div>
+  </header>
+  <header class="show-for-small-only-up hide-for-large-up">
+    <nav class="top-bar" id="frontpage-topbar" data-topbar role="navigation">
+      <ul class="title-area">
+        <li class="name">
+        </li>
+        <li class="toggle-topbar menu-icon"><a href="#"><span></span></a></li>
+      </ul class="right">
+      <section class="top-bar-section">
+        <ul class="right">
+            <li class='<?= $css_class ?>'>
+              <a href='index.php?page=<?= $navigation_item[1] ?>'>
+               
+              </a>
+            </li>
+        </ul>
+      </section>
+    </nav>
+  </header>
+  
+  <h1>HI!!!!</h1>
+
+  <!-- build:js ../scripts/scripts_bottom.min.js -->
+  <script src="../js/foundation.min.js"></script>
+  <script src="../js/vendor/fastclick.js"></script>
+  <script src="../js/vendor/placeholder.js"></script>
+  <script src="../js/vendor/jquery.coockie.js"></script>
+  <!-- endbuild -->
+
+  <script>
+    $(document).foundation();
+  </script>
+</body>
+</html>
