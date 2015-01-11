67a46b7be26915f111d2d34178e0b27ee7337215
pdoblhoff <philippdoblhoff@Philipps-MacBook-Air.local>
2015-01-11 20:55:54 +0100

    logn redirect

--- html/dev/_frontend/sites/login.php
+++ html/dev/_frontend/sites/login.php
@@ -2,32 +2,15 @@
   <div class="row text-center">
     <a href="index.php"><img src="../img/logo_symbol.png" class="logo half-padding" alt="logo"></a>
     <h1 class="padding-bottom">Login</h1>
-    <dl class="tabs" data-tab data-options="deep_linking:true">
-      <dd class="active logintabs"><a href="#webworker">Webworker</a></dd>
-      <dd class="logintabs"><a href="#kunde">Kunde</a></dd>
-    </dl>
-    <div class="tabs-content">
-      <div class="content active" id="webworker">
         <form class="loginform" action="../_worker" method="POST">
-          <input type="email" placeholder="E-mail" />
-          <input type="password" placeholder="Passwort" />
+          <input type="email" id="email" placeholder="E-mail" value="a@a.at" />
+          <input type="password" id="password" placeholder="Passwort" value="3uiC8Mag" />
           <input type="submit" class="button expand padding-top" id="loginbutton" value="Login">
         </form>
         <div id="loginlinks" class="padding-top" >
           <a href="index.php?page=register" id="reglink"><span data-tooltip aria-haspopup="true" class="has-tip" title="zur Registrierung"><i class="fa fa-plus fa-fw"></i></span></a>
           <a href="" data-reveal-id="help" id="helplink"><span data-tooltip aria-haspopup="true" class="has-tip" title="Passwort widerherstellen"><i class="fa fa-question fa-fw"></i></span></a>
         </div>
-      </div>
-      <div class="content" id="kunde">
-        <form class="loginform" action="../_client" method="POST">
-          <input type="email" placeholder="E-mail" />
-          <input type="password" placeholder="Passwort" />
-          <input type="submit" class="button expand padding-top" id="loginbutton" value="Login">
-        </form>
-        <div id="loginlinks" class="padding-top" >
-         <a href="" data-reveal-id="help" id="helplink"><span data-tooltip aria-haspopup="true" class="has-tip" title="Passwort widerherstellen"><i class="fa fa-question fa-fw"></i></span></a>
-       </div>
-     </div>
    </div>
  </div>
 </div>
@@ -41,7 +24,22 @@
       <input type="text" id="remail" placeholder="mustermann@gmail.com">
     </div>
   </div>
-
   <a class="close-reveal-modal">&#215;</a>
   <a href="" class="button green-bg text-right"><i class="fa fa-save fa-fw"></i> Speichern</a>
 </div>
+
+<script>
+var email = $('#email').val();
+var password = $('#password').val();
+
+  $.post( "/calculi/rest/auth/login", { email: email, password: password })
+  .done(function( data ) {
+    console.log(data)
+    if (data.success == "true") {
+      window.location = "/calculi/html/dev/_worker/index2.php";
+    }
+    else {
+      alert("keine Berechtigung");
+    }
+  });
+</script>
